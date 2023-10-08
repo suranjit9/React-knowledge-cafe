b8bfb48115babfe6c23f2094d723e804a8e74414
Suranjit Mondal <suranjitm07@gmail.com>
2023-10-08 06:56:29 +0600

    Header Created

---
+++ src/assets/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div className="flex justify-between border-b-2 p-2 mx-auto">
+            <h1 className="text-3xl text-blod pt-2">Knowledge-Cafe</h1>
+            <img src="/src/images/profile.png" alt="" />
+        </div>
+    );
+};
+
+export default Header;
