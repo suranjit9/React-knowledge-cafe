6246d5ad5a91dba662d49e7531cefed8cd1c4e35
Suranjit Mondal <suranjitm07@gmail.com>
2023-10-08 09:11:51 +0600

    created card

--- src/assets/Blogs/blog.jsx
+++ src/assets/Blogs/blog.jsx
@@ -1,16 +1,31 @@
 
 
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
     console.log(blog);
-    const {title, img,authorimg,authorname,postiongdate}=blog;
+    const { title, img, authorimg, authorname, postiongdate, time } = blog;
     return (
-        <div>
-            <img src={img} alt="" />
-            <div>
-                <img src={authorimg} alt="" />
-                <h2>{authorname}</h2>
-                <p>{postiongdate}</p>
+        <div className="space-y-2 md:space-y-3 mb-5">
+            <img className="w-full rounded-lg" src={img} alt="" />
+
+            <div className="flex justify-between mx-auto">
+                <div className="flex">
+                    <img className="w-14" src={authorimg} alt="" />
+                    <div className="pl-2 ">
+                        <h2 className="font-semibold text-xl">{authorname}</h2>
+                        <p className="text-base">{postiongdate}</p>
+                    </div>
+                </div>
+                <div>
+                    <h2>{time} Munits</h2>
+                </div>
             </div>
+            <div>
+            <h1 className="text-2xl font-semibold md:text-4xl font-semibold">{title}</h1>
+            <p>#NewesBangla #blogenew</p>
+            </div>
+            <div className="text-end"><button className="text-orange-700 underline underline-offset-4">Red More</button></div>
+            
+            
         </div>
     );
 };
